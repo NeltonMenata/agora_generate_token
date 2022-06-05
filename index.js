79aba2a652026648a65c1eb5fc8fc7775959faad
NeltonMenata <neltonmenataditec@gmail.com>
2022-06-05 13:09:30 +0100

    Excessões adicionadas

--- index.js
+++ index.js
@@ -18,11 +18,29 @@ app.post("/rtctoken", (req, res)=>{
     const appId = process.env.APPID || "";
     const appCertificate = process.env.APPCERTIFICATE || "";
     const uid = Math.floor(Math.random() * 100000 );
+
+    // Tempo de Expiração - 1 hora em segundos
     const expirationTimeSeconds = 3600;
+
+    // Tempo actual
     const currentTimestamp = Math.floor(Date.now() / 1000);
+    
+    // Carimbo de tempo para expirar o token
     const privilegeExpiretedTs = expirationTimeSeconds + currentTimestamp;
+    
+    // Papel do Usuário - PUBLICADOR || SUBSCRITOR
     const role = req.body.isPublisher ? Agora.RtcRole.PUBLISHER : Agora.RtcRole.SUBSCRIBER;
+    
+    
     const channel = req.body.channel;
+    if(channel == "" || channel == null){
+        //res.send({"error":"not definied channel!"}); //json({"error":"the channel was not definied!"})
+        throw("Error channel not definied!");
+    }else if(appId == "" || appId == null){
+        throw("Error appId not definied!");
+    }else if(appCertificate == "" || appCertificate == null){
+        throw("Error appCertificate not definied!");
+    }
 
     const token = Agora.RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channel, uid, role, privilegeExpiretedTs);
     res.send({
